bd06430cc5abeadb24484079d1c743185add4eba
rooney123777 <rooney123777@gmail.com>
2022-08-02 13:04:54 +0300

    version 1.0.0 release

---
+++ app/src/main/java/com/github/rooneyandshadows/materialpopupmenu/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.rooneyandshadows.materialpopupmenu.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.rooneyandshadows.materialpopupmenu.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
